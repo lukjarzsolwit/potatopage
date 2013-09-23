2a5a84fc3e12b4bfc5f171915291919c17001e40
Armin Brunner <brunner.armin@gmail.com>
2013-09-23 11:21:46 +0100

    Using the same function for multiple things… I hope that works, otherwise we should split the generic bit off.

--- templatetags/paginator_tags.py
+++ templatetags/paginator_tags.py
@@ -4,13 +4,20 @@ from django import template
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
+def add_to_query_string(context, key, value):
+    """ Adds a query to the query string if it exists, otherwise it creates a new one """
+    query_string = context['request'].GET.copy()
+    query_string[key] = value
+    return query_string.urlencode()
+
+
+@register.simple_tag(takes_context=True)
 def paginator_querystring(context, page_number, page_name):
     """ Formats the querystring for the given page number, but keeps the rest of the querystring in tact.
         The page_name argument handles multiple paginators in the same template. The default one is 'page'.
     """
-    query_dict = context['request'].GET.copy()
-    query_dict[page_name] = page_number
-    return query_dict.urlencode()
+    return add_to_query_string(context, 'page_name', page_number)
+
 
 @register.simple_tag(takes_context=True)
 def set_page_name(context):
@@ -19,12 +26,6 @@ def set_page_name(context):
         context['page_name'] = 'page'
     return ''
 
-@register.simple_tag(takes_context=True)
-def add_to_query_string(context, key, value):
-    """ Adds a query to the query string if it exists, otherwise it creates a new one """
-    query_string = context['request'].GET.copy()
-    query_string[key] = value
-    return query_string.urlencode()
 
 @register.simple_tag
 def paginator_total_object_count(page):
